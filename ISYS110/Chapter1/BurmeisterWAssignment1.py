7937828a5f8461dbe1a5f420e74a610f3247aef9
burmeiw <98549233+burmeiw@users.noreply.github.com>
2022-01-27 13:00:35 -0500

    Add files via upload

---
+++ ISYS110/Chapter1/BurmeisterWAssignment1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Hello world, this is a programmin trial")
+print("my name is Wyatt")
+print("thus far this class is fun")
