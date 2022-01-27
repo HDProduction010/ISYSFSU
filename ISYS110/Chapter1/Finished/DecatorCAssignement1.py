44384a7372ce0ab85761d757c55964e848be1370
HDProduction010 <50345144+HDProduction010@users.noreply.github.com>
2022-01-27 15:11:49 -0500

    Add files via upload

---
+++ ISYS110/Chapter1/Finished/DecatorCAssignement1.py
@@ -0,0 +1,3 @@
+print('Hello World! This is a thing for Python I hate python 3 due to the issues I had with it on Node.py and trying to depoly a bot for a application called disord')
+print('Well as well that I kinda just dont like to set up something in python 3 with dependences')
+print('In short summarty I hate python 3')
